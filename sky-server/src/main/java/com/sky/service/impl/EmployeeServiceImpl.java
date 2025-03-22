c4bf02e7b31e34cf0126f47a1316b125c7f6c1b1
Useless-Noob-hub <316464342@qq.com>
2025-03-22 18:57:21 +0800

    启用禁用员工账号业务代码

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -103,4 +103,16 @@ public class EmployeeServiceImpl implements EmployeeService {
         return pageResult;
     }
 
+    @Override
+    public void startOrStop(Integer status, Long id) {
+/*        Employee employee = new Employee();
+        employee.setStatus(status);
+        employee.setId(id);*/
+        Employee employee = Employee.builder()
+                .status(status)
+                .id(id)
+                .build();
+        employeeMapper.update(employee);
+    }
+
 }
