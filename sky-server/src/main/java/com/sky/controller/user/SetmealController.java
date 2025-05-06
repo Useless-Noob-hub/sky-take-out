196a037d7b11e90f5fced1282183d162bc833770
Useless-Noob-hub <316464342@qq.com>
2025-05-06 11:34:23 +0800

    feat(cache): 实现套餐数据缓存功能
    - 在 SetmealController 中添加缓存注解，实现套餐数据的缓存
    - 在用户端 SetmealController 中添加缓存注解，实现套餐列表的缓存
    - 在 SkyApplication 中启用缓存功能
    - 优化套餐数据的操作，确保缓存的一致性

--- sky-server/src/main/java/com/sky/controller/user/SetmealController.java
+++ sky-server/src/main/java/com/sky/controller/user/SetmealController.java
@@ -8,6 +8,7 @@ import com.sky.vo.DishItemVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -27,6 +28,7 @@ public class SetmealController {
      */
     @GetMapping("/list")
     @ApiOperation("根据分类id查询套餐")
+    @Cacheable(cacheNames = "setmealCache",key = "#categoryId" )//key: setneal::100
     public Result<List<Setmeal>> list(Long categoryId) {
         Setmeal setmeal = new Setmeal();
         setmeal.setCategoryId(categoryId);
@@ -48,15 +50,5 @@ public class SetmealController {
         List<DishItemVO> list = setmealService.getDishItemById(id);
         return Result.success(list);
     }
-    /**
-     * 批量删除套餐
-     * @param ids
-     * @return
-     */
-    @DeleteMapping
-    @ApiOperation("批量删除套餐")
-    public Result delete(@RequestParam List<Long> ids){
-        setmealService.deleteBatch(ids);
-        return Result.success();
-    }
+
 }
